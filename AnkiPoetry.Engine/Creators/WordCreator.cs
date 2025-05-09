e25fdb4e8c11b17f03b1257f1d2f464b76edb55f
MarshmellowCat <acct.on1@proton.me>
2025-05-09 16:45:09 +0200

    Update WordCreator.cs

--- AnkiPoetry.Engine/Creators/WordCreator.cs
+++ AnkiPoetry.Engine/Creators/WordCreator.cs
@@ -121,7 +121,7 @@ public partial class WordCreator : BaseCreator<Card>
                 word += "...";
 
 //removing annoying [word] prompt in NextLineInitial cloze
-            sb.Append($"{{{{c{cloze_num}::{word}::w}}}}");
+            sb.Append($"{{{{c{cloze_num}::{word}:: w }}}}");
 
             cloze_num++;
         }
