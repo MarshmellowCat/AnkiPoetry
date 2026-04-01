1c0ec7d4a01561f6298a1af7a6e401970c7f75eb
MarshmellowCat <acct.on1@proton.me>
2026-04-02 01:52:43 +0200

    Update WordCreator.cs

--- AnkiPoetry.Engine/Creators/WordCreator.cs
+++ AnkiPoetry.Engine/Creators/WordCreator.cs
@@ -11,7 +11,7 @@ public partial class WordCreator : BaseCreator<Card>
 {
     protected override IEnumerable<Card> CardFromChunk(Chunk chunk, Parameters parameters)
     {
-        for (var i = -1; i < chunk.Lines.Length; ++i)
+        for (var i = 0; i+1 < chunk.Lines.Length; ++i)
         {
             var to = chunk.Lines[i];
 
