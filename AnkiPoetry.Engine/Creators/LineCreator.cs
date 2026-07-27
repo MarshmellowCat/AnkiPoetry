e76083698084e71d19e90becfbabdb0d033fcbeb
MarshmellowCat <acct.on1@proton.me>
2026-07-27 20:47:42 +0200

    Update LineCreator.cs

--- AnkiPoetry.Engine/Creators/LineCreator.cs
+++ AnkiPoetry.Engine/Creators/LineCreator.cs
@@ -70,8 +70,8 @@ public class LineCreator : BaseCreator<Card>
                 //var number = CreateNumber(chunk.MaxSongNumber, chunk.SectionNumber, chunk.SongNumber, to.LineNumber);
 
 
-                var beginning = CreateHeader(chunk, parameters) + FirstWordJoinLines(chunk.Lines[..(i + 1)], parameters);
-                // var beginning = CreateHeader(chunk, parameters);
+                //var beginning = CreateHeader(chunk, parameters) + FirstWordJoinLines(chunk.Lines[..(i + 1)], parameters);
+                var beginning = CreateHeader(chunk, parameters);
 
                // if (to.IsFirst)
                    // beginning += "<hr>";
